30ee050cb0fb1b75704ef209a7e6f1314d54b53b
yogeshbiradar123 <114044104+yogeshbiradar123@users.noreply.github.com>
2022-09-28 11:50:57 +0530

    Add files via upload

---
+++ table1.java
@@ -0,0 +1,17 @@
+//wap to print table of a given number 
+import java.util.*;
+
+class table1
+{
+public static void main(String args[])
+{
+Scanner s= new Scanner(System.in);
+
+System.out.println("eneter number");
+
+int n=s.nextInt();
+
+//for( int t=1;t<=10;t++)    System.out.println(n +" * " + t +"="+ n*t);
+
+
+}}
